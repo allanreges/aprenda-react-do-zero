38d374b30ca7804ca8595e7d6980e867f3853e52
Allan <allan_creative@hotmail.com>
2023-06-02 15:29:30 -0300

    finish initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
-import './globals.css'
+import './globals.css';
+import StyledComponentsRegistry from '../../lib/registry';
+import { Roboto } from 'next/font/google';
 
+const roboto = Roboto({
+  weight: ['300', '400', '500'],
+  subsets: ['latin']
+})
+ 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
-  )
+  );
 }
